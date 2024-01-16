1a8f4fb044ec856769ab2c2be67e81b10005e631
kkratos <kamal.patel30@gmail.com>
2024-01-16 17:23:05 -0500

    flask app

--- src/utils.py
+++ src/utils.py
@@ -10,7 +10,6 @@ from src.exception import CustomException
 from sklearn.metrics import r2_score
 from sklearn.model_selection import GridSearchCV
 
-
 def save_object(file_path, obj):
     
     try:
@@ -51,5 +50,14 @@ def evaluate_models(X_train, y_train, X_test, y_test, models, param):
             report[list(models.keys())[i]] = test_model_score
 
         return report
+    except Exception as e:
+        raise CustomException(e, sys)
+    
+
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return dill.load(file_obj)
+
     except Exception as e:
         raise CustomException(e, sys)
