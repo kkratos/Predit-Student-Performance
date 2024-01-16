a837be95f71bec413d4dd6252184b135e4d2d90e
kkratos <kamal.patel30@gmail.com>
2024-01-16 10:32:06 -0500

    Data Transformation done

--- src/exception.py
+++ src/exception.py
@@ -8,7 +8,7 @@ def error_message_details(error, error_detail:sys):
     
     return error_message
 
-class CustomerException(Exception):
+class CustomException(Exception):
     def __init__(self, error_message, error_detail):
         super().__init__(error_message)
 
